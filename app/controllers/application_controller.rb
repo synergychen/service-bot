4556c935c9c3423464591dc32706376aa0f51401
Chen Huang <alan.tolearn@gmail.com>
2016-04-10 21:44:55 -0400

    Add auth for every request to server
    - request to server should contain token generated based on "email:password"

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_filter :authenticate_user_from_token, except: [:token]
+
   include ActionController::HttpAuthentication::Basic::ControllerMethods
   include ActionController::HttpAuthentication::Token::ControllerMethods
 
@@ -14,6 +16,14 @@ class ApplicationController < ActionController::Base
       else
         render json: { error: 'Incorrect credentials' }, status: 401
       end
+    end
+  end
+
+  private
+
+  def authenticate_user_from_token
+    unless authenticate_with_http_token { |token, options| User.find_by(auth_token: token) }
+      render json: { error: 'Bad Token'}, status: 401
     end
   end
 end
